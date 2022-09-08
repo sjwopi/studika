a17e054f4ad24c7390fe1862b3ab10559240034e
Ivan Teteruk <iteterk@gmail.com>
2022-09-08 10:34:16 -0200

    сделал popup геолокации

--- skripts/index.js
+++ skripts/index.js
@@ -8,6 +8,7 @@ let hamburger = header.querySelector('.header__mobile-hamb');
 let searchContainer = header.querySelector('.header__search');
 let searchPopUpBtn = header.querySelector('.header__search-btn');
 let searchPopUpClose = header.querySelector('.header__search-close');
+let geoBtn = header.querySelector('.header__geo');
 
 let menu = document.querySelector('.menu');
 let menuList = menu.querySelector('.menu__list');
@@ -15,6 +16,10 @@ let arrowRight = menu.querySelector('.right-arrow');
 let arrowLeft = menu.querySelector('.left-arrow');
 let backGradientRight = menu.querySelector('.menu__back-gradient_right');
 let backGradientLeft = menu.querySelector('.menu__back-gradient_left');
+
+let popUpGeo = document.querySelector('.popup-geo');
+let popUpGeoSelectBtn = popUpGeo.querySelector('.popup-geo__select-button');
+let popUpGeoCloseBtn = popUpGeo.querySelector('.popup-geo__close-button');
 
 function checkWidth() {
     if (screenWidth <= 1400) {
@@ -38,12 +43,19 @@ function searchPopUp() {
 function animationNone() {
     searchContainer.classList.toggle('header__search_animation_none');
 }
-checkWidth();
+function geoPopUp() {
+    popUpGeo.classList.toggle('active');
+    popUpGeoCloseBtn.classList.toggle('active');
+    backPopUp.classList.toggle('active');
+}
+checkWidth();popUpGeoSelectBtn
+geoBtn.addEventListener('click', geoPopUp);
+popUpGeoSelectBtn.addEventListener('click', geoPopUp);
+popUpGeoCloseBtn.addEventListener('click', geoPopUp);
 
 searchPopUpBtn.addEventListener('click', searchPopUp);
 searchContainer.addEventListener('click', animationNone);
 searchPopUpClose.addEventListener('click', function two() {searchPopUp(); animationNone();});
-
 
 arrowRight.addEventListener('click', skrollMenu);
 arrowLeft.addEventListener('click', skrollMenu);
